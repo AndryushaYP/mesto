ed16dd89f213496afde3aa577438be442ec2bb9f
Andrey Kloninger <veto.aion@gmail.com>
2020-07-24 01:49:14 +0500

    fix: вынес массив в отдельный файл, разделил функции, исправил секцию profile на моб разрешениях

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-const cardTemplate = document.querySelector('#card-template').content;
-
 const mainPopup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_add');
@@ -9,7 +7,7 @@ const buttonEdit = document.querySelector('.profile__button_edit');
 const buttonAdd = document.querySelector('.profile__button_add');
 const buttonCloseFormAdd = popupAdd.querySelector('.popup__button-close');
 const buttonCloseFormEdit = popupEdit.querySelector('.popup__button-close');
-const buttonClosePopupOpenImage = popupImage.querySelector('.popup__button-close');
+const buttonClose = popupImage.querySelector('.popup__button-close');
 
 const nameInput = document.querySelector('.popup__info_name');
 const professionInput = document.querySelector('.popup__info_profession');
@@ -18,22 +16,30 @@ const linkInput = popupAdd.querySelector('.popup__info_link');
 
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-
-// Открытие/закрытие попапа
-
-function toggleModal (popup) {
-    popup.classList.toggle('popup_opened');
-}
-
-
 const titlePopupOpenImage = popupImage.querySelector('.popup__caption');
 const urlPopupOpenImage = popupImage.querySelector('.popup__image');
 
+const cardList = document.querySelector('.cards__list');
+
+function toggleModal (popup) {
+
+    popup.classList.toggle('popup_opened');
+
+}
+
 buttonEdit.addEventListener('click', () => toggleModal(popupEdit));
-buttonAdd.addEventListener('click', () => toggleModal(popupAdd));
+
+buttonAdd.addEventListener('click', function () {
+
+    titleInput.value = '';
+    linkInput.value = '';
+    toggleModal (popupAdd);
+
+});
+
 buttonCloseFormAdd.addEventListener('click', () => toggleModal(popupAdd));
 buttonCloseFormEdit.addEventListener('click', () => toggleModal(popupEdit));
-buttonClosePopupOpenImage.addEventListener('click', () => toggleModal(popupImage));
+buttonClose.addEventListener('click', () => toggleModal(popupImage));
 
 function formSubmitHandler (evt) {
 
@@ -46,6 +52,35 @@ function formSubmitHandler (evt) {
 
 popupEdit.addEventListener('submit', formSubmitHandler);
 
+function createCard () {
+
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__caption');
+    const buttonLike = cardElement.querySelector('.card__button-like');
+    const buttonDelete = cardElement.querySelector('.card__delete');
+
+    cardImage.src = linkInput.value;
+    cardTitle.textContent = titleInput.value;
+
+    buttonLike.addEventListener('click', likeCard);
+
+    buttonDelete.addEventListener('click', function(){
+        const listItem = buttonDelete.closest('.cards__list-item');
+        listItem.remove();
+    });
+    
+    cardImage.addEventListener('click', function() {
+        toggleModal(popupImage);
+        titlePopupOpenImage.textContent = cardTitle.textContent;
+        urlPopupOpenImage.src = cardImage.src;
+    });
+
+    cardList.prepend(cardElement);
+}
+
 //Лайк
 
 function likeCard(evt) {
@@ -57,93 +92,18 @@ function likeCard(evt) {
 function addCard(evt) {
     
     evt.preventDefault();
-    
-    const cardElement = cardTemplate.cloneNode(true);
 
-    const cardImage =  cardElement.querySelector('.card__image');
-    cardImage.src = linkInput.value;
-    const cardTitle = cardElement.querySelector('.card__caption');
-    cardTitle.textContent = titleInput.value;
+    createCard();
 
-    const buttonLike = cardElement.querySelector('.card__button-like');
-
-    buttonLike.addEventListener('click', likeCard);
-
-    const buttonDelete = cardElement.querySelector('.card__delete');
-
-    buttonDelete.addEventListener('click', function(){
-        const listItem = buttonDelete.closest('.cards__list-item');
-        listItem.remove();
-    });
-
-    cardImage.addEventListener('click', function() {
-        toggleModal(popupImage);
-        titlePopupOpenImage.textContent = cardTitle.textContent;
-        urlPopupOpenImage.src = cardImage.src;
-    });
-
-    cardList.prepend(cardElement);
     toggleModal(popupAdd);
 }
 
 popupAdd.addEventListener('submit', addCard);
 
-// Вы писали "Данный массив можно вынести в отдельный файл utils.js  и подключить его к основному файлу скриптов 
-// ( в данном случае index.js). Это позволит сделать размер index.js меньше 
-// и в дальнейшем упростит работу с моковыми данными." Я не нашёл способ сделать это без подключения сторонних 
-// библиотек/сборщиков. Нашел функцию, которая делает это, но отзывы о ней "не очень".
-
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-const cardList = document.querySelector('.cards__list');
-
 initialCards.forEach((data) => {
 
-    const cardElement = cardTemplate.cloneNode(true);
+    linkInput.value = data.link;
+    titleInput.value = data.name;
 
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__caption');
-    const buttonLike = cardElement.querySelector('.card__button-like');
-    const buttonDelete = cardElement.querySelector('.card__delete');
-    buttonLike.addEventListener('click', likeCard);
-    buttonDelete.addEventListener('click', function(){
-        const listItem = buttonDelete.closest('.cards__list-item');
-        listItem.remove();
-    });
-    
-    cardImage.addEventListener('click', function() {
-        toggleModal(popupImage);
-        titlePopupOpenImage.textContent = cardTitle.textContent;
-        urlPopupOpenImage.src = cardImage.src;
-    });
-
-    cardImage.src = data.link;
-    cardTitle.textContent = data.name;
-  
-    cardList.prepend(cardElement);
+    createCard();
 });
