968808c83a04cbeb13b2f1650e1e46dbdb099102
Andrey Kloninger <veto.aion@gmail.com>
2020-08-01 20:14:29 +0500

    fix: разбил функцию на неск частей

--- scripts/validate.js
+++ scripts/validate.js
@@ -7,18 +7,17 @@ const myObject = {
     errorClass: 'popup__error_visible'
 }
 
-const enableValidation = ({formSelector, inputSelector, submitButtonSelector, 
-    inactiveButtonClass, inputErrorClass, errorClass}) => {
-    const formList = Array.from(document.querySelectorAll(formSelector));
-    formList.forEach((formElement) => {
-        formElement.addEventListener('submit', (evt) => {
-            evt.preventDefault();
-        });
+//Проверка валидности полей
+
+const setEventListener = (formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass}) => {
 
     const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+
     const buttonSubmit = formElement.querySelector(submitButtonSelector);
-        console.log(inputList);
-    inputList.forEach((inputElement) => {
+
+    console.log(inputList);
+
+        inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', (evt) => {
             const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
             console.log(errorElement);
@@ -31,17 +30,51 @@ const enableValidation = ({formSelector, inputSelector, submitButtonSelector,
                 errorElement.classList.add(errorClass);
                 errorElement.textContent = inputElement.validationMessage;
             }
-            buttonSubmit.disabled = true;
-            const isFormValid = inputList.some((inputElement) => !inputElement.validity.valid);
-            if(!isFormValid) {
-                buttonSubmit.classList.remove(inactiveButtonClass);
-                buttonSubmit.disabled = false;
-            } else {
-                buttonSubmit.classList.add(inactiveButtonClass);
-                buttonSubmit.disabled = true;
-            }
+            
+            toggleButtonState(inputList, buttonSubmit, {inactiveButtonClass});
         })
     })
+
+}
+
+// Невалидность поля
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+      });
+    }
+
+
+//Валидация кнопки
+
+const toggleButtonState = (inputList, buttonSubmit, {inactiveButtonClass}) => {
+
+            if(hasInvalidInput(inputList)) {
+                buttonSubmit.classList.add(inactiveButtonClass);
+                buttonSubmit.disabled = true;
+                
+            } else {
+                buttonSubmit.classList.remove(inactiveButtonClass);
+                buttonSubmit.disabled = false;
+            }
+        }
+
+
+//Включение валидации
+
+const enableValidation = ({formSelector, inputSelector, submitButtonSelector, 
+    inactiveButtonClass, inputErrorClass, errorClass}) => {
+
+    const formList = Array.from(document.querySelectorAll(formSelector));
+
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+
+    setEventListener(formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass});
+
     })
 }
 
