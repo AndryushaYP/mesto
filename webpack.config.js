6e787cb1688c8a408dea78766386bd27295c4820
Andrey Kloninger <veto.aion@gmail.com>
2020-09-07 00:06:22 +0500

    подключил к проекту Webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: [
+                MiniCssExtractPlugin.loader, 
+                { loader: 'css-loader', options: { importLoaders: 1 } },
+                'postcss-loader'
+            ],
+          }, {
+            test: /\.(png|jpe?g|gif|woff2|woff|ttf|svg)$/i,
+            use: [
+              {
+                loader: 'file-loader'
+              },
+            ],
+          },
+        ]
+      },
+      plugins: [
+          new HtmlWebpackPlugin({
+              template: './src/index.html'
+          }),
+          new MiniCssExtractPlugin()
+        ]
+};
